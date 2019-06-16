eaf814d828656180a808dd3c57410233aab79e46
Sébastien Gousseau <sebastiengousseau@MacBook-Pro-de-Sebastien.local>
2019-06-16 16:37:40 +0200

    Project init & pods

---
+++ CodingTest_Meteo/CodingTest_Meteo/UI & Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodingTest_Meteo
+//
+//  Created by Sébastien Gousseau on 16/06/2019.
+//  Copyright © 2019 Sébastien Gousseau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodingTest_Meteo/Pods/Target Support Files/Pods-CodingTest_Meteo/Pods-CodingTest_Meteo-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CodingTest_Meteo : NSObject
+@end
+@implementation PodsDummy_Pods_CodingTest_Meteo
+@end
